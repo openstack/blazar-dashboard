6ba6089aa1c45e393929fad51d51d7774adae09f
Hiroaki Kobayashi <kobayashi.hiroaki@lab.ntt.co.jp>
2018-02-15 15:57:39 +0900

    Use the openstackdocstheme for release notes
    Change-Id: I305f2290660fd322214f6ee5ff07469c6bd33683

--- releasenotes/source/conf.py
+++ releasenotes/source/conf.py
@@ -39,7 +39,13 @@
 # ones.
 extensions = [
     'reno.sphinxext',
+    'openstackdocstheme'
 ]
+
+# openstackdocstheme options
+repository_name = 'openstack/blazar-dashboard'
+bug_project = 'blazar'
+bug_tag = ''
 
 # Add any paths that contain templates here, relative to this directory.
 # templates_path = ['_templates']
@@ -106,7 +112,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'openstackdocs'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
