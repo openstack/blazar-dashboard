36cf34a3e9ef8f6d2d5dd77d3f97ac388cf57893
Alex Rocha <rochaa@tacc.utexas.edu>
2018-10-17 16:14:36 -0500

    updating help text with instuctions for creating single day leases

--- blazar_dashboard/content/leases/forms.py
+++ blazar_dashboard/content/leases/forms.py
@@ -61,7 +61,7 @@ class CreateForm(forms.SelfHandlingForm):
     number_of_days = forms.IntegerField(
         label=_("Reservation Length in Days"),
         required=False,
-        help_text=_('Number of days for this reservation.'),
+        help_text=_('Set to zero to schedule leases starting and ending on the same day'),
         initial=1,
         error_messages={
             'invalid': _('Value should be a whole number'),
