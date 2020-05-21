a75724b2df4c79b8a4fb445102eadb0dc7867d30
Andreas Jaeger <aj@suse.com>
2020-05-21 20:18:09 +0200

    Switch to newer openstackdocstheme and reno versions
    Switch to openstackdocstheme 2.2.1 and reno 3.1.0 versions. Using
    these versions will allow especially:
    * Linking from HTML to PDF document
    * Allow parallel building of documents
    * Fix some rendering problems
    
    Update Sphinx version as well.
    
    Set openstackdocs_pdf_link to link to PDF file. Note that
    the link to the published document only works on docs.openstack.org
    where the PDF file is placed in the top-level html directory. The
    site-preview places the PDF in a pdf directory.
    
    Disable openstackdocs_auto_name to use 'project' variable as name.
    
    Change pygments_style to 'native' since old theme version always used
    'native' and the theme now respects the setting and using 'sphinx' can
    lead to some strange rendering.
    
    openstackdocstheme renames some variables, so follow the renames
    before the next release removes them. A couple of variables are also
    not needed anymore, remove them.
    
    See also
    http://lists.openstack.org/pipermail/openstack-discuss/2020-May/014971.html
    
    Change-Id: I882ab18020ee8a15a3204e72d1bf68f4d5195b14

--- releasenotes/source/conf.py
+++ releasenotes/source/conf.py
@@ -43,9 +43,10 @@ extensions = [
 ]
 
 # openstackdocstheme options
-repository_name = 'openstack/blazar-dashboard'
-bug_project = 'blazar'
-bug_tag = ''
+openstackdocs_repo_name = 'openstack/blazar-dashboard'
+openstackdocs_auto_name = False
+openstackdocs_bug_project = 'blazar'
+openstackdocs_bug_tag = ''
 
 # Add any paths that contain templates here, relative to this directory.
 # templates_path = ['_templates']
@@ -99,7 +100,7 @@ exclude_patterns = []
 # show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = 'native'
 
 # A list of ignored prefixes for module index sorting.
 # modindex_common_prefix = []
