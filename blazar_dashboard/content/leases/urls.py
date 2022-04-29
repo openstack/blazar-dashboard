28ec6c6fb627632cfb1571fd1c4a869c5a95629f
manchandavishal <manchandavishal143@gmail.com>
2022-04-29 18:39:49 +0530

    Address RemovedInDjango40Warning
    In Django 3.1, django.conf.urls.url() is deprecated
    in favor of django.urls.re_path(). For more info see [1]
    These were already replaced in Horizon repo by [2].
    
    [1] https://docs.djangoproject.com/en/4.0/releases/3.1/#id2
    [2] https://review.opendev.org/c/openstack/horizon/+/827093
    
    Change-Id: I71e21300e6c072f790ae0517bb40d18b01a4a23b

--- blazar_dashboard/content/leases/urls.py
+++ blazar_dashboard/content/leases/urls.py
@@ -14,24 +14,24 @@
 #    under the License.
 
 from blazar_dashboard.content.leases import views as leases_views
-from django.conf.urls import url
+from django.urls import re_path
 
 LEASE_URL = r'^(?P<lease_id>[^/]+)/%s$'
 
 urlpatterns = [
-    url(r'^calendar/(?P<resource_type>(host|device|network))/$', leases_views.CalendarView.as_view(), name='calendar'),
-    url(r'^calendar/(?P<resource_type>(host|device|network))/resources\.json$', leases_views.calendar_data_view,
+    re_path(r'^calendar/(?P<resource_type>(host|device|network))/$', leases_views.CalendarView.as_view(), name='calendar'),
+    re_path(r'^calendar/(?P<resource_type>(host|device|network))/resources\.json$', leases_views.calendar_data_view,
         name='calendar_data'),
 
-    url(r'^(?P<resource_type>[^/]+)/extras\.json$',
+    re_path(r'^(?P<resource_type>[^/]+)/extras\.json$',
         leases_views.extra_capabilities,
         name='extra_capabilities'),
 
-    url(r'^$', leases_views.IndexView.as_view(), name='index'),
-    url(r'^create/$', leases_views.CreateView.as_view(), name='create'),
-    url(LEASE_URL % '', leases_views.DetailView.as_view(),
+    re_path(r'^$', leases_views.IndexView.as_view(), name='index'),
+    re_path(r'^create/$', leases_views.CreateView.as_view(), name='create'),
+    re_path(LEASE_URL % '', leases_views.DetailView.as_view(),
         name='detail'),
-    url(LEASE_URL % 'update', leases_views.UpdateView.as_view(),
+    re_path(LEASE_URL % 'update', leases_views.UpdateView.as_view(),
         name='update'),
-    url(LEASE_URL % 'reallocate', leases_views.ReallocateView.as_view(), name='reallocate'),
+    re_path(LEASE_URL % 'reallocate', leases_views.ReallocateView.as_view(), name='reallocate'),
 ]
