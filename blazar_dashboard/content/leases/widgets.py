6e8a940929548d8298614333b437d3a84d0cc3ed
Mark Powers <markpowers@uchicago.edu>
2022-06-06 14:28:13 +0000

    Add renderer kwarg to custom widgets
    This was a change in Django 2.1.
    
    Change-Id: I0dd1232e18f907c8fc88de84cce717bad1d63a68

--- blazar_dashboard/content/leases/widgets.py
+++ blazar_dashboard/content/leases/widgets.py
@@ -32,7 +32,7 @@ class CapabilityWidget(Widget):
             'switchable_class': self.switchable_class
         }}
 
-    def render(self, name, value, attrs=None):
+    def render(self, name, value, attrs=None, renderer=None):
         context = self.get_context(name, value, attrs)
         template = loader.get_template(self.template_name.format(
             resource_type=self.resource_type)).render(context)
@@ -93,7 +93,7 @@ class TimespanWidget(Widget):
             'value': value,
         }}
 
-    def render(self, name, value, attrs=None):
+    def render(self, name, value, attrs=None, renderer=None):
         context = self.get_context(name, value, attrs)
         template = loader.get_template(self.template_name).render(context)
         return mark_safe(template)
