59c1b456595b0a9a389a2f364789a736b8094121
Adam Cooper <adamcooper@uchicago.edu>
2022-08-11 18:06:14 -0500

    Revert "Add filter header to host calendar"
    This reverts commit 6ba4da43b4215c27142fd93ffcff18cba67c9fa1.

--- blazar_dashboard/static/leases/js/calendar/lease_chart.js
+++ blazar_dashboard/static/leases/js/calendar/lease_chart.js
@@ -73,16 +73,6 @@
   var calendarElement = $(selector);
   var projectId; // Used to avoid showing restricted devices if this project is authorized
 
-  function updateFilterHeader(chooser)  {
-    const header = $("#blazar-calendar-filter-header");
-    const value = chooser.value;
-    if (value === undefined || value.toLowerCase() !== "all hosts") {
-        header.textContent = "Showing all hosts";
-    } else {
-        header.textContent = `Showing all ${value} hosts`;
-    }
-  }
-
   function init() {
     var chart; // The chart object
     var allReservations;
@@ -152,18 +142,12 @@
           });
           chooser.empty();
           chooser.append(new Option(`${gettext("All")} ${pluralResourceType}`, '*'));
-          populateChooser(chooser, availableResourceTypes);
-          if ($("#blazar-calendar-host").length !== 0) {
-            updateFilterHeader(chooser);
-          }
+          populateChooser(chooser, availableResourceTypes)
           Object.keys(availableResourceTypes).forEach(function (key) {
             chooser.append(new Option(key, key));
           });
           chooser.prop('disabled', false);
           chooser.change(function () {
-            if ($("#blazar-calendar-host").length !== 0) {
-              updateFilterHeader(chooser);
-            }
             var chosenType = $('#resource-type-chooser').val();
             filteredReservations = allReservations.map(function (reservation) {
               var reservationCopy = Object.assign({}, reservation)
